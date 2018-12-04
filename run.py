6e66c5892d510ee72dd3f1d329e0f2d240722821
Carlos <bortolotti.carlos@gmail.com>
2018-12-04 14:57:50 -0200

    Integração com Oracle

--- run.py
+++ run.py
@@ -7,14 +7,24 @@ for d in remote.get_pending():
 
     package = []
 
-    db = OracleDatabase(
-        d['sqlConnection']['connectionString'],
-        d['sqlConnection']['user'],
-        d['sqlConnection']['password']
-        )
-    
-    for i in db.get_cursor(d['sqlQuery'], []):
-        print(i)
+    try:
 
-    #print("Sending -> ", d['code'], ", Status -> ", remote.send_data(package))
-    print("Sending -> ", d)
+        db = OracleDatabase(
+            d['sqlConnection']['connectionString'],
+            d['sqlConnection']['user'],
+            d['sqlConnection']['password']
+            )
+
+        cursor = db.get_cursor(d['sqlQuery'], [])
+        column_names = [row[0] for row in cursor.description]
+        
+        for c in cursor:
+            data = [{k : v } for k, v in zip(column_names, c)]
+            print(data)
+            
+
+        #print("Sending -> ", d['code'], ", Status -> ", remote.send_data(package))
+        #print("Sending -> ", d)
+
+    except:
+        print("Error on query ->", d['sqlQuery'])
